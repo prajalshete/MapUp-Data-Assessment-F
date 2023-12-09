7dd4e5fac637c223d8785dc4274e65b02908fc89
prajalshete <140526280+prajalshete@users.noreply.github.com>
2023-12-09 12:45:42 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -47,46 +47,91 @@ def get_bus_indexes(df)->list:
     return list()
 
 
-def filter_routes(df)->list:
-    """
-    Filters and returns routes with average 'truck' values greater than 7.
 
-    Args:
-        df (pandas.DataFrame)
 
-    Returns:
-        list: List of route names with average 'truck' values greater than 7.
+def filter_routes(df: pd.DataFrame) -> list:
+    
     """
+    # Group by 'route' and calculate the average 'truck' values
+    route_avg_truck = df.groupby('route')['truck'].mean()
+
+    # Filter routes with average 'truck' values greater than 7
+    filtered_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
+
+    return filtered_routes
+
+# Example usage
+# Assuming 'dataset-1.csv' is the name of your CSV file
+df = pd.read_csv('dataset-1.csv')
+result_filtered_routes = filter_routes(df)
+print(result_filtered_routes)
+
+
+
+    # Write your logic here
+    
+
+def custom_multiply(value):
+    """
+    Custom function to multiply matrix values based on conditions.
+    Modify this function based on your specific conditions.
+    """
+    if value < 0:
+        return value * 2
+    elif value >= 0 and value <= 5:
+        return value * 3
+    else:
+        return value
+
+def multiply_matrix(matrix: pd.DataFrame) -> pd.DataFrame:
+    
+    # Apply the custom_multiply function to each element of the matrix
+    modified_matrix = matrix.applymap(custom_multiply)
+
+    return modified_matrix
+
+# Example usage
+# Assuming 'your_matrix.csv' is the name of your CSV file containing the matrix
+matrix_df = pd.read_csv('your_matrix.csv', index_col=0)
+result_modified_matrix = multiply_matrix(matrix_df)
+print(result_modified_matrix)
+
+
+
+
     # Write your logic here
 
-    return list()
-
-
-def multiply_matrix(matrix)->pd.DataFrame:
+def time_check(df: pd.DataFrame) -> pd.Series:
     """
-    Multiplies matrix values with custom conditions.
+    Verify the completeness of the data by checking whether the timestamps
+    for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period.
 
     Args:
-        matrix (pandas.DataFrame)
+        df (pandas.DataFrame): Input DataFrame
 
     Returns:
-        pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
+        pd.Series: Boolean series indicating completeness for each (`id`, `id_2`) pair.
     """
-    # Write your logic here
+    # Convert the timestamp column to datetime type
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-    return matrix
+    # Calculate the time range for 24 hours
+    time_range_24_hours = pd.to_timedelta(1, unit='D')
 
+    # Calculate the time range for 7 days
+    time_range_7_days = pd.to_timedelta(7, unit='D')
 
-def time_check(df)->pd.Series:
-    """
-    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
+    # Group by unique (`id`, `id_2`) pairs and check completeness
+    completeness_check = df.groupby(['id', 'id_2'])['timestamp'].apply(
+        lambda x: (x.max() - x.min() >= time_range_24_hours) and (x.max() - x.min() >= time_range_7_days)
+    )
 
-    Args:
-        df (pandas.DataFrame)
+    return completeness_check
 
-    Returns:
-        pd.Series: return a boolean series
-    """
-    # Write your logic here
+# Example usage
+# Assuming 'dataset-2.csv' is the name of your CSV file
+df = pd.read_csv('dataset-2.csv')
+result_completeness_check = time_check(df)
+print(result_completeness_check)
 
-    return pd.Series()
+    
